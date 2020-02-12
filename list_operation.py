cd1484ada0bde6a5cee2a992f3849568c0eb53b3
RAJESHKUMAR SIVAPRAKASAM <43309655+Rajeshkumar-sivaprakasam@users.noreply.github.com>
2020-02-12 08:07:08 +0530

    Update list_operation.py

--- list_operation.py
+++ list_operation.py
@@ -24,8 +24,36 @@ if __name__ == '__main__':
 		args = s[1:]
 		if cmd != "print":
 			eval('l.{0}{1}'.format(cmd,tuple(map(int,args))))
-			"""cmd += "("+ ",".join(args) +")"
+			
+		"""cmd += "("+ ",".join(args) +")"
 	    	eval("l."+cmd)
 	    	"""
 		else:
 			print(l)
+
+"""
+Output Format
+
+For each command of type print, print the list on a new line.
+
+Sample Input 0
+
+12
+insert 0 5
+insert 1 10
+insert 0 6
+print
+remove 6
+append 9
+append 1
+sort
+print
+pop
+reverse
+print
+Sample Output 0
+
+[6, 5, 10]
+[1, 5, 9, 10]
+[9, 5, 1]
+"""
